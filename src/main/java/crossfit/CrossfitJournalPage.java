e2f01cc6e193f40f82dafbba5d6e19288e748814
yeodimri <yeodimri@gmail.com>
2018-10-27 21:32:41 +0300

    another changes from today

--- src/main/java/crossfit/CrossfitJournalPage.java
+++ src/main/java/crossfit/CrossfitJournalPage.java
@@ -20,7 +20,6 @@ public class CrossfitJournalPage extends CrossfitAbstractPage {
     private static final By2 bannerTopOverlayText = new By2("banner Top Overlay Text", By.xpath(("//*[@id=\"bannerTopOverlayText\"]")));
     private static final By2 catagoryDropDown = new By2("Catagory DropDown", By.xpath("//*[@id=\"media-filter\"]/div[1]/select"));
     private static final By2 languageyDropDown = new By2("Language DropDown", By.xpath("//*[@id=\"media-filter\"]/div[2]/select"));
-    //    private static final By2 journalSerchButton = new By2("Journal Serch Button", By.xpath("//*[@id=\"media-filter\"]/input[2]"));
     private static final By2 searchFieldJournalPage = new By2("Journal Search field", By.xpath("//*[@id=\"media-filter\"]/input[1]"));
     private static final By2 searchButtonJournalPage = new By2("Journal Search Button", By.xpath("//*[@id=\"media-filter\"]/input[2]"));
     private String upperCatagory;
